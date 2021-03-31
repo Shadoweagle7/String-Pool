ed96f344eaa35f3f87c85453c4dddeb2a2d6b09d
Shadoweagle7 <38882778+Shadoweagle7@users.noreply.github.com>
2021-03-31 19:54:01 +1100

    See description
    Fixed this->pool when pool is a static variable.
    
    Added an allocator, but haven't done anything with it yet. It was planned for std::set in an attempt to be able to do something like std::string a = ""; and have that automatically add to the string pool instead, but that failed. This code is supposed to adhere to the standard library, and creating a custom string class to adhere to that isn't necessary but possible in the future.

--- String Pool/String Pool/String Pool.cpp
+++ String Pool/String Pool/String Pool.cpp
@@ -7,17 +7,78 @@
 #include <utility>
 #include <concepts>
 
+#define STATIC_CLASS(x)\
+    x ## () = delete;\
+    x ## (const x &) = delete;\
+    x ## (x &&) = delete;
+
 class string_pool {
+public:
+    struct allocator;
 private:
     static std::set<std::string> pool;
 public:
+
+    STATIC_CLASS(string_pool)
+
+    struct allocator {
+    private:
+        std::allocator<std::string> internal_allocator;
+    public:
+        using value_type = std::allocator<std::string>::value_type;
+        using size_type = std::allocator<std::string>::size_type;
+        using difference_type = std::allocator<std::string>::difference_type;
+        using propagate_on_container_move_assignment =
+            std::allocator<std::string>::propagate_on_container_move_assignment;
+
+        constexpr allocator() noexcept {}
+        constexpr allocator(const allocator &a) noexcept {}
+        constexpr allocator(allocator &&a) noexcept {}
+
+        // address() is deprecated in C++17, removed in C++20
+
+        [[nodiscard]] /* constexpr C++20 */ value_type *allocate(size_t n) {
+            return this->internal_allocator.allocate(n);
+        }
+
+        /* constexpr C++20 */ void deallocate(value_type *p, size_t n) {
+            this->internal_allocator.deallocate(p, n);
+        }
+
+        // max_size(), construct() and destroy() are deprecated in C++17, removed in C++20
+
+        bool operator==(const allocator &other) const noexcept {
+            return true;
+        }
+
+        bool operator!=(const allocator &other) const noexcept {
+            return false;
+        }
+
+        /* constexpr (waiting for complete C++20 support, MSVC) */  ~allocator() {}
+    };
+
+    static size_t size() {
+        return pool.size();
+    }
+
     template<class... argv>
-    const char *make(argv&&... args) requires std::constructible_from<std::string, argv...> {
-        this->pool.emplace(std::forward(args)...).first->c_str();
+    static const char *make(argv&&... args) requires std::constructible_from<std::string, argv...> {
+        return pool.emplace(std::forward<argv>(args)...).first->c_str();
     }
 };
 
+std::set<std::string> string_pool::pool;
+
 int main(int argc, const char *argv[]) {
+    const char *test = string_pool::make("boing");
+
+    std::cout << "string_pool::size(): " << string_pool::size() << "\n";
+
+    const char *test2 = string_pool::make("boing");
+
+    std::cout << "string_pool::size(): " << string_pool::size() << "\n";
+
     return 0;
 }
 
